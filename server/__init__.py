c1ff4e257ffe92f352c2b801622c353cbec68610
EAR <e.avila@rdc-consultores.com>
2021-04-11 23:06:07 -0500

    enable frontend on web app

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from .config import config
 
-from .routes.raices.routes import raices
+from .blueprints.raices.routes import raices
+from .blueprints.client.routes import client
 
 
 def create_app(*args):
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="spa", static_url_path="/spa")
     app.config.from_object(config)
 
     register_blueprints(app)
@@ -16,3 +17,4 @@ def create_app(*args):
 
 def register_blueprints(app):
     app.register_blueprint(raices)
+    app.register_blueprint(client)
